294044a62047129ae0dd69b4a4a0cef4f398a635
JDhyeok <jdhyeok97@gmail.com>
2021-05-10 13:05:41 +0900

    feat: add login activity

---
+++ app/src/main/java/com/example/together/LoginActivity.java
@@ -0,0 +1,84 @@
+package com.example.together;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
+    private FirebaseAuth mAuth;
+    private FirebaseUser user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
+
+        setContentView(R.layout.activity_login);// UI view
+        findViewById(R.id.signUpBtn).setOnClickListener(onClickListener);
+        findViewById(R.id.loginBtn).setOnClickListener(onClickListener);
+
+    }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v){
+            switch(v.getId()){
+                case R.id.loginBtn:
+                    authLogin();
+                    if(user != null){
+                        startHomeActivity();
+                    }
+                    break;
+                case R.id.signUpBtn:
+                    startSignUpActivity();
+                    //회원가입화면 이동
+                    break;
+            }
+        }
+    };
+
+    private void authLogin() {
+        String email = ((EditText)findViewById(R.id.userID)).getText().toString();
+        String password = ((EditText)findViewById(R.id.userPW)).getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            user = mAuth.getCurrentUser();
+                            Toast.makeText(getApplicationContext(),"Login Success",Toast.LENGTH_LONG).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(getApplicationContext(),"Login Failed",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    private void startHomeActivity() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+    private void startSignUpActivity() {
+        Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+        startActivity(intent);
+    }
+}
