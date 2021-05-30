0fd3137a5492e45ab42e0ecf5aa0df9cae07b553
Hyeon-uk-kang <jaron0920@naver.com>
2021-05-30 19:47:12 +0900

    link

---
+++ app/src/main/java/com/example/together/activities/TvingbbsActivity.java
@@ -0,0 +1,14 @@
+package com.example.together.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TvingbbsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_to_tving_bbs);
+    }
+}
