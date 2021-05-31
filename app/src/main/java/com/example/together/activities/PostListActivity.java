cc75ebf5ced753b12b90bad47a959b74a56397a3
JDhyeok <jdhyeok97@gmail.com>
2021-05-31 21:09:02 +0900

    feat: add create post

---
+++ app/src/main/java/com/example/together/activities/PostListActivity.java
@@ -0,0 +1,30 @@
+package com.example.together.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class PostListActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mRef;
+    private Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance();
+        mRef = mDatabase.getReference("posts");
+
+
+    }
+
+
+}
