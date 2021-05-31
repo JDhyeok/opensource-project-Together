32a235819fffd5a79d1cb8d98d04001fd10b1f41
JDhyeok <jdhyeok97@gmail.com>
2021-06-01 01:41:41 +0900

    chore: add post list activity

--- app/src/main/java/com/example/together/activities/PostListActivity.java
+++ app/src/main/java/com/example/together/activities/PostListActivity.java
@@ -1,20 +1,43 @@
 package com.example.together.activities;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.together.R;
+import com.example.together.adapter.PostAdapter;
+import com.example.together.models.Post;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PostListActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<Post> postList;
 
     private FirebaseAuth mAuth;
     private FirebaseDatabase mDatabase;
     private DatabaseReference mRef;
     private Toolbar mToolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -23,6 +46,33 @@ public class PostListActivity extends AppCompatActivity {
         mDatabase = FirebaseDatabase.getInstance();
         mRef = mDatabase.getReference("posts");
 
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        postList = new ArrayList<>();
+
+        mDatabase = FirebaseDatabase.getInstance();
+        mRef = mDatabase.getReference("posts");
+        mRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                postList.clear();
+                for(DataSnapshot snap : snapshot.getChildren()) {
+
+                    Post post = snap.getValue(Post.class);
+                    postList.add(post);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e(TAG, "에러럴러러러러러러러럴러러ㅓ");
+            }
+        });
+
+        adapter = new PostAdapter(postList, this);
+        recyclerView.setAdapter(adapter);
 
     }
 
