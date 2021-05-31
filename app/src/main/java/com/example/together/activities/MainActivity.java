c7c14ee1d803b6b81079dba6fb0194fce1769b8d
JDhyeok <jdhyeok97@gmail.com>
2021-05-31 23:18:55 +0900

    chore: add post id to xml file

--- app/src/main/java/com/example/together/activities/MainActivity.java
+++ app/src/main/java/com/example/together/activities/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
 
         netflix=findViewById(R.id.netflix);
         netflix.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/together/activities/PostActivity.java
+++ app/src/main/java/com/example/together/activities/PostActivity.java
@@ -7,14 +7,18 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.together.R;
 import com.example.together.models.Post;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -102,9 +106,39 @@ public class PostActivity extends AppCompatActivity{
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 
+    private Post posts;
+
     public String getUserName(String UID){
         // 현재 사용자 이름 가져오기
-        DatabaseReference userName = mRef.child("user").child(UID).child("username");
-        return userName.toString();
+
+
+        mRef.equalTo(UID).addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                posts = snapshot.getValue(Post.class);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                posts = snapshot.getValue(Post.class);
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+                posts = snapshot.getValue(Post.class);
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                posts = snapshot.getValue(Post.class);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+
+
+        return posts.getWriter().toString();
     }
 }
