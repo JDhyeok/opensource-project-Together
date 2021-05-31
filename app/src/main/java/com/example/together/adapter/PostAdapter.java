d89c3ac1d3db4a8fd58b4f7412572c22016854ae
JDhyeok <jdhyeok97@gmail.com>
2021-06-01 01:42:22 +0900

    chore: add Post Adapter for post list

---
+++ app/src/main/java/com/example/together/adapter/PostAdapter.java
@@ -0,0 +1,58 @@
+package com.example.together.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.together.R;
+import com.example.together.models.Post;
+
+import java.util.ArrayList;
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostViewHolder> {
+
+
+    private ArrayList<Post> postList;
+    private Context context;
+
+    public PostAdapter(ArrayList<Post> postList, Context context){
+        this.postList = postList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public PostViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.아이템리스트, parent, false);
+        PostViewHolder holder = new PostViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PostViewHolder holder, int position) {
+        holder.title.setText(postList.get(position).getTitle());
+        holder.title.setText(postList.get(position).getContent());
+    }
+
+    @Override
+    public int getItemCount() {
+        return (postList != null ? postList.size() : 0);
+    }
+
+    public class PostViewHolder extends RecyclerView.ViewHolder {
+        TextView title;
+        TextView content;
+
+        public PostViewHolder(@NonNull View postView) {
+            super(postView);
+            this.title = itemView.findViewById(R.id.타이틀아이디);
+            this.content = itemView.findViewById(R.id.내용아이디);
+        }
+    }
+}
